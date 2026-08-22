69a0ff2e15f07025c42a5244b47e3ecb18048917
MD Hamid Raza <shaikhhamidraza98710@gmail.com>
2026-08-22 17:11:47 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,45 +1,20 @@
-n=document.querySelectorAll(".drum");
-for(var i=0;i<n.length;i++){
-n[i].addEventListener("click", handclick);
+var nume1 = Math.random();
+var nume2 = Math.random();
+var num1 = Math.floor(nume1 * 6) + 1;
+var num2 = Math.floor(nume2 * 6) + 1;
+document.querySelector(".img1").setAttribute("src","./images/dice"+num1+".png");
+document.querySelector(".img2").setAttribute("src","./images/dice"+num2+".png");
+function Result(){
+if(num1===num2) {
+   document.querySelector("h1").textContent="Draw";
+   }
+else if(num1>num2){
+    document.querySelector("h1").textContent="🚩 Player 1 Wins!"; 
+     }
+else{
+    document.querySelector("h1").textContent="Player 2 Wins! 🚩";
+}            
 }
-function handclick(){
-  var innerbuttonHTML=this.innerHTML;  
- mykey(innerbuttonHTML);
-}
-
-document.addEventListener("keypress",function(event){
-  mykey(event.key);});
-  function mykey(key){
-   switch(key){
-case "w": 
-var ton1=new Audio("./sounds/tom-1.mp3");
-ton1.play();
-break; 
-case "a": 
-var ton2=new Audio("./sounds/tom-2.mp3");
-ton2.play();
-break; 
-case "s": 
-var ton3=new Audio("./sounds/tom-3.mp3");
-ton3.play();
-break;
-case "d": 
-var ton4=new Audio("./sounds/tom-4.mp3");
-ton4.play();
-break;
-case"j": 
-var snare =new Audio("./sounds/snare.mp3");
-snare.play();
-break; 
-case "k": 
-var crash=new Audio("./sounds/crash.mp3");
-crash.play();
-break; 
-case "l": 
-var kick=new Audio("./sounds/kick-bass.mp3");
-kick.play();
-break;  
-default :
-console.log(key);
-} 
-  }
+console.log(num1);
+console.log(num2);
+console.log(Result());
